14833afd123a0439391616d3e7d2bf548e151296
betanandes <robertafernandes46@gmail.com>
2024-07-25 19:31:54 -0300

    contando tentativas

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let numeroSecreto = 7;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 //boa prática que evita repetição de códigos
 function exibirTextoNaTela(tag, texto) {
@@ -7,13 +8,23 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
 
     if (chute == numeroSecreto) {
-        exibirTextoNaTela('h1', 'Acertou!')
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas++;
     }
 }
 
